d6a17b21f7bfe19cc20fd2d2850eaf2c192a7a12
dindiongco <dindiongco@qa.com>
2021-08-18 23:26:36 +0100

    DI: Added update task Selenium Test.

--- BackEnd/src/test/java/com/qa/index/IndexPage.java
+++ BackEnd/src/test/java/com/qa/index/IndexPage.java
@@ -13,12 +13,35 @@ public class IndexPage {
 	@FindBy(id = "create-button")
 	private WebElement createBtn;
 	
+	@FindBy(id = "edit-task")
+	private WebElement editBtn;
+	
+	@FindBy(id = "update-button")
+	private WebElement updateBtn;
+	
+	@FindBy(id = "delete-task")
+	private WebElement deleteBtn;
+	
 	@FindBy(id = "task-title")
 	private WebElement taskTitle;
+	
+	@FindBy(xpath = "/html/body/section/div/div[2]")
+	private WebElement cardDiv;
 	
 	public void create(String task) {
 		createTask.sendKeys(task);
 		createBtn.click();
+	}
+	
+	public void update(String task) {
+		editBtn.click();
+		createTask.clear();
+		createTask.sendKeys(task);
+		updateBtn.click();
+	}
+	
+	public void delete() {
+		deleteBtn.click();
 	}
 	
 	public String checkTask() {
--- BackEnd/src/test/java/com/qa/selenium/IndexTest.java
+++ BackEnd/src/test/java/com/qa/selenium/IndexTest.java
@@ -1,6 +1,5 @@
 package com.qa.selenium;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.concurrent.TimeUnit;
@@ -16,38 +15,50 @@ import org.openqa.selenium.support.PageFactory;
 import com.qa.index.IndexPage;
 
 public class IndexTest {
-	
+
 	private WebDriver driver;
-	
+
 	@BeforeEach
 	public void setup() {
-		
+
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
-		
-		driver = new ChromeDriver();
 		
 		this.driver = new ChromeDriver();
 		this.driver.manage().window().maximize();
-		
+
 		Timeouts timeouts = driver.manage().timeouts();
-		timeouts.implicitlyWait(5, TimeUnit.SECONDS); 
+		timeouts.implicitlyWait(5, TimeUnit.SECONDS);
 		timeouts.pageLoadTimeout(15, TimeUnit.SECONDS);
 	}
-	
+
 	@Test
 	public void testCreate() {
-		
+
 		String testString = "Get tea";
-		
+
 		IndexPage index = PageFactory.initElements(driver, IndexPage.class);
+
+		this.driver.get(index.URL);
+
+		index.create(testString);
+
+		assertThat(index.checkTask().toLowerCase().contains(testString));
+	}
+
+	@Test
+	public void testUpdate() {
 		
+		String testString = "Meditate";
+
+		IndexPage index = PageFactory.initElements(driver, IndexPage.class);
+
 		this.driver.get(index.URL);
 		
-		index.create(testString);
+		index.update(testString);
 		
 		assertThat(index.checkTask().toLowerCase().contains(testString));
 	}
-	
+
 	@AfterEach
 	public void teardown() {
 		this.driver.quit();
